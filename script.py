2e0eed4038e5d872334d87d34f0d08490da61666
maiurysousa <git>
2022-05-05 12:22:49 -0300

    adicionando método de resultados de pesquisa

--- script.py
+++ script.py
@@ -9,3 +9,6 @@ driver.get("https://www.google.com")
 campo = driver.find_element_by_xpath("//input[@aria-label='Pesquisar']") # Acha o campo de pesquisa - Propriedade e valor da bara de pesquisa precisam estar bem especificados
 campo.send_keys(pesquisa) # Método para digitar a pesquisa 
 campo.send_keys(Keys.ENTER) # Aperta o Enter
+
+resultados = driver.find_element_by_xpath("//div[@id='result-stats']") #Pega o texto de resultados
+print(resultados.text) # printa resultados encontrados
