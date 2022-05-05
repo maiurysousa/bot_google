2cd36fa0b376adb35488d74bf94df01e398baadc
maiurysousa <git>
2022-05-05 17:33:05 -0300

    criando método de navegação entre as páginas

--- script.py
+++ script.py
@@ -15,3 +15,15 @@ print(resultados) # printa resultados encontrados
 numero_resultados = int(resultados.split("Aproximadamente ")[1].split(' resultados')[0].replace('.','')) # Função paraecortar resultados
 maximo_paginas = numero_resultados / 10
 print("Número de páginas: %s" % (maximo_paginas))
+
+url_pagina = driver.find_element_by_xpath("//a[@aria-label='Page 2']").get_attribute("href") # Pegar a propriedade e o atributo href
+
+# Estrutura de repetição - passando para a próxima página
+pagina_atual = 0
+start = 10 # Pois são 10 resultados por pág
+while pagina_atual <=10: 
+    if not pagina_atual == 0:
+        url_pagina = url_pagina.replace("start = %s" % start, "start = %s" % (start+10)) # substitui a url_pagina por +10
+        start += 10 # Soma mais 10 no start da página atual
+    pagina_atual += 1 # Página atual
+    driver.get(url_pagina) #Abrindo a próxima pág
